9f40e1c6ae602a5e94dba1ef451fdf8b3d801714
Beni <benjaminjaichenco@gmail.com>
2023-05-31 20:43:38 +0300

    add createForm method

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,4 +1,23 @@
 import Project from "./project.js";
 import Task from "./task.js";
 
-export default class Display {}
+export default class Display {
+  static createForm() {
+    const body = document.body;
+    const container = document.createElement("div");
+    container.classList.add(".container");
+
+    const formElement = document.createElement("form");
+    formElement.setAttribute("action", "#");
+    formElement.setAttribute("method", "get");
+
+    const titleInput = document.createElement("input");
+    titleInput.classList.add("task-title-input");
+    titleInput.setAttribute("type", "text");
+    titleInput.setAttribute("name", "title");
+    titleInput.setAttribute("placeholder", "title");
+
+    container.appendChild(formElement);
+    formElement.appendChild(titleInput);
+  }
+}
