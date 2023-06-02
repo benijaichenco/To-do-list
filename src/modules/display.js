02fb9fd45d4df8ceca8d032947b43dd3928b254d
Beni <benjaminjaichenco@gmail.com>
2023-06-02 14:13:29 +0300

    create header and load header

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,24 +1,63 @@
 import Project from "./project.js";
 import Task from "./task.js";
+import logo from "../images/logo.png";
+import plusSign from "../images/plus-circle.png";
 
 export default class Display {
-  static createForm() {
+  static loadHomePage() {
+    Display.getHeader();
+  }
+
+  static getHeader() {
     const body = document.body;
-    const container = document.createElement("div");
-    container.classList.add(".container");
 
-    const formElement = document.createElement("form");
-    formElement.setAttribute("action", "#");
-    formElement.setAttribute("method", "get");
+    // create header
+    const header = document.createElement("header");
+    header.classList.add("header");
+    body.appendChild(header);
 
-    const titleInput = document.createElement("input");
-    titleInput.classList.add("task-title-input");
-    titleInput.setAttribute("type", "text");
-    titleInput.setAttribute("name", "title");
-    titleInput.setAttribute("placeholder", "title");
+    // left header part
+    const headerLeft = document.createElement("div");
+    headerLeft.classList.add("header-left");
+    header.appendChild(headerLeft);
 
-    body.appendChild(container);
-    container.appendChild(formElement);
-    formElement.appendChild(titleInput);
+    const logoImg = document.createElement("img");
+    logoImg.classList.add("logo");
+    logoImg.setAttribute("src", logo);
+    headerLeft.appendChild(logoImg);
+
+    const logoText = document.createElement("h1");
+    logoText.classList.add("text-logo");
+    logoText.textContent = "To-Do";
+    headerLeft.appendChild(logoText);
+
+    // right header part
+    const headerRight = document.createElement("div");
+    headerRight.classList.add("header-right");
+    header.appendChild(headerRight);
+
+    const addProjectBtn = document.createElement("button");
+    addProjectBtn.classList.add("add-project");
+    addProjectBtn.removeEventListener("click", Project.addProject);
+    addProjectBtn.addEventListener("click", Project.addProject);
+    headerRight.appendChild(addProjectBtn);
+
+    const textContainer = document.createElement("div");
+    textContainer.classList.add("text-container");
+    addProjectBtn.appendChild(textContainer);
+
+    const addProjectBtnText = document.createElement("div");
+    addProjectBtnText.classList.add("add-project-button-text");
+    addProjectBtnText.textContent = "Add Project";
+    textContainer.appendChild(addProjectBtnText);
+
+    const plusSignSpan = document.createElement("span");
+    plusSignSpan.classList.add("plus-sign");
+    addProjectBtn.appendChild(plusSignSpan);
+
+    const plusSignImg = document.createElement("img");
+    plusSignImg.classList.add("add-project-img");
+    plusSignImg.setAttribute("src", plusSign);
+    plusSignSpan.appendChild(plusSignImg);
   }
 }
